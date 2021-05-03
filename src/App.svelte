83eef942d30244b3c3fb2245606dc96dee4b090a
MartaLourido <martalouri@gmail.com>
2021-05-04 00:31:36 +0200

    created basic header with logo

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,18 @@
 <script>
-	export let name;
+  import Header from "./components/Header.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-</main>
+<Header />
+<h1 class="heading">Humanitera</h1>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .heading {
+    text-transform: uppercase;
+    text-align: center;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let src = "https://www.linkpicture.com/q/logo_219.png";
+</script>
+
+<nav>
+  <img class="logo" {src} alt="humanitera logo" />
+</nav>
+
+<style>
+  nav {
+    box-sizing: border-box;
+    background: #83b271;
+    padding: 10px 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 70px;
+  }
+  .logo {
+    text-align: left;
+    width: 80px;
+    padding: 15px;
+    color: #fff;
+  }
+</style>
